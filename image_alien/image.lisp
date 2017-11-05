a7ef961bbc610ee06d5cbf3a4eb448cf0c53eca3
Galen Helfter <ghelfter@gmail.com>
2017-11-04 21:40:18 -0400

    Added image conversion from C to Lisp and the Lisp image class

---
+++ image_alien/image.lisp
@@ -0,0 +1,98 @@
+;;;; *********************************************************
+;;;;                       Galen Helfter
+;;;;                         DPA 8150
+;;;;                         Project 1
+;;;;                        image.lisp
+;;;; *********************************************************
+
+;;; Package definition
+;;(defpackage :composite
+;;  (:use :common-lisp))
+(in-package :composite)
+
+;;; Generics
+(defgeneric access-pixel (img x y channel)
+  (:documentation "Given an x and y, accesses the image at that
+                  plus the given channel."))
+
+(defgeneric access-pixels-sdim (img i channel)
+  (:documentation "Accesses a pixel at a given, single dimensional index."))
+
+(defgeneric image-size (img)
+  (:documentation "Given an image, calculates its total size."))
+
+(defgeneric image-dim (img)
+  (:documentation "Given an image, calculates the width by the height."))
+
+(defgeneric clear-channel (img channel value)
+  (:documentation "Given an image and a channel, clears all of the channel
+                  to the given value."))
+
+;;; Class definition
+(defclass image ()
+  ((width
+     :initarg :width
+     :initform (error "Must specify a width")
+     :accessor width
+     :type fixnum
+     :documentation "Width of the image.")
+   (height
+     :initarg :height
+     :initform (error "Must specify a height")
+     :accessor height
+     :type fixnum
+     :documentation "Height of the image.")
+   (channels
+     :initarg :channels
+     :initform (error "Must specify channels")
+     :accessor channels
+     :type fixnum
+     :documentation "Channels of the image.")
+   (data
+     :initform nil
+     :accessor data
+     :type (simple-array single-float)
+     :documentation "Data where the image is stored.")))
+
+;;; Methods
+(defmethod initialize-instance :after ((img image) &key)
+  (setf (slot-value img 'data) (make-array
+                                 (* (width img)
+                                    (the fixnum (* (height img)
+                                    (channels img))))
+                                 :element-type 'single-float
+                                 :initial-element 0f0)))
+
+(defmethod access-pixel ((img image) x y channel)
+  (declare (type fixnum x y channel))
+  (aref (slot-value img 'data) (+ channel
+                                 ( + (the fixnum (* x (channels img)))
+                                     (the fixnum
+                                       (the fixnum (* (the fixnum (* y (width img)))
+                                                      (channels img)))))))) 
+(defmethod access-pixels-sdim ((img image) i channel)
+  (declare (type fixnum i channel))
+  (aref (slot-value img 'data)
+       (+ channel (the fixnum(* i (channels img))))))
+
+(defmethod image-size ((img image))
+  (* (channels img) (the fixnum (* (width img) (height img)))))
+
+(defmethod image-dim ((img image))
+  (* (width img) (height img)))
+
+(defmethod clear-channel ((img image) channel value)
+  (declare (type fixnum channel) (type single-float value))
+  (loop for i of-type fixnum from 0 to (- (image-dim img) 1) do
+    (setf (aref (slot-value img 'data)
+               (+ (* i (channels img)) channel)) value)))
+
+;; Type declarations for optimization
+(declaim (ftype (function (image) fixnum) width))
+(declaim (ftype (function (image) fixnum) height))
+(declaim (ftype (function (image) fixnum) channels))
+(declaim (ftype (function (&rest t) single-float) access-pixel))
+(declaim (ftype (function (&rest t) single-float) access-pixel-sdim))
+(declaim (ftype (function (&rest t) (simple-array single-float)) data))
+(declaim (ftype (function (image) fixnum) image-size))
+(declaim (ftype (function (image) fixnum) image-dim))
---
+++ image_alien/image_alien.lisp
@@ -0,0 +1,88 @@
+;;;; *********************************************************
+;;;;                       Galen Helfter
+;;;;                         DPA 8150
+;;;;                         Project 1
+;;;;                      image_alien.lisp
+;;;; *********************************************************
+
+;;; Package definition
+;;(defpackage :composite
+;;  (:use :common-lisp))
+(in-package :composite)
+
+;;; Alien types definitions
+(sb-alien:define-alien-type nil
+  (sb-alien:struct ImageStore
+    (data (* (sb-alien:single-float)))
+    (w sb-alien:unsigned-int)
+    (h sb-alien:unsigned-int)
+    (channels sb-alien:unsigned-int)))
+
+;;; Foreign function definitions
+(sb-alien:define-alien-routine image_load sb-alien:integer
+  (img (* (sb-alien:struct ImageStore)))
+  (filename sb-alien:c-string))
+
+(sb-alien:define-alien-routine image_write sb-alien:integer
+  (img (* (sb-alien:struct ImageStore)))
+  (filename sb-alien:c-string))
+
+(sb-alien:define-alien-routine image_cleanup sb-alien:void
+  (img (* (sb-alien:struct ImageStore))))
+
+(defmacro with-image-read (var fname &body body)
+  `(let ((,var (sb-alien:make-alien (sb-alien:struct ImageStore))))
+     (image_load ,var ,fname)
+     (unwind-protect (progn ,@body)
+       (image_cleanup ,var)
+       (sb-alien:free-alien ,var))))
+
+(defun convert-to-c-image (img)
+  (declare (optimize (speed 3) (safety 0)))
+  (declare (type image img))
+  (let ((istruct (sb-alien:make-alien (sb-alien:struct ImageStore))))
+    (setf (sb-alien:slot istruct 'data) (sb-alien:make-alien
+                                           sb-alien:single-float
+                                           (image-size img)))
+    (setf (sb-alien:slot istruct 'w) (width img))
+    (setf (sb-alien:slot istruct 'h) (height img))
+    (setf (sb-alien:slot istruct 'channels) (channels img))
+    (loop for i from 0 to (- (image-size img) 1) do
+      (setf (sb-alien:deref (sb-alien:slot istruct 'data) i)
+            (elt (data img) i)))
+    istruct))
+
+(defmacro with-image-write (var fname img &body body)
+  `(let ((,var (convert-to-c-image ,img)))
+     (image_write ,var ,fname)
+     (unwind-protect (progn ,@body)
+       (image_cleanup ,var)
+       (sb-alien:free-alien ,var))))
+
+(defun convert-from-c-image (istruct img)
+  (declare ((sb-alien:alien (sb-alien:struct ImageStore)) istruct))
+  (declare (type image img))
+  (declare (optimize (speed 3) (safety 0)))
+  (setf (slot-value img 'width) (sb-alien:slot istruct 'w))
+  (setf (slot-value img 'height) (sb-alien:slot istruct 'h))
+  (setf (slot-value img 'channels) (sb-alien:slot istruct 'channels))
+  (setf (slot-value img 'data) (make-array (* (width img)
+                                              (height img)
+                                              (channels img))
+                                           :element-type 'single-float
+                                           :initial-element 0f0))
+  (loop for i from 0 to (- (image-size img) 1) do
+    (setf (elt (slot-value img 'data) i) (sb-alien:deref
+                                 (sb-alien:slot istruct 'data) i))))
+
+(defun read-image (filename img)
+  (declare (optimize (speed 3) (safety 0)))
+  (declare (simple-string filename))
+  (declare (type image img))
+  (with-image-read istruct filename
+    (convert-from-c-image istruct img)))
+
+(defun write-image (filename img)
+  (declare (simple-string filename))
+  (declare (type image img))
+  (with-image-write istruct filename img ()))
